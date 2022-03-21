f05370efaf05399d771ab3efc68b1c6c9b97a97c
gamal <gassuntamal2@gmail.com>
2022-03-21 10:14:50 -0700

    ADDED a mian.h file

---
+++ 0x05-pointers_arrays_strings/main.h
@@ -0,0 +1,14 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+
+/**
+ *File: main.h
+ *Header file containing prototypes for all functions used in thi 0X05-pointers_arrays_strings directory
+ *
+ */
+
+#include <stdlib.h>
+
+
+#endif
