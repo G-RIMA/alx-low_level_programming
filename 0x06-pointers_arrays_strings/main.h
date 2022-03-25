164046667234956164b451d805f1d77b6bc57403
gamal <gassuntamal2@gmail.com>
2022-03-25 00:36:17 -0700

    infinite add

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -19,4 +19,7 @@ char *leet(char *n);
 char *rot13(char *s);
 void print_number(int n);
 int main(void);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+char *add_strings(char *n1, char *n2, char *r, int r_index);
+
 #endif
