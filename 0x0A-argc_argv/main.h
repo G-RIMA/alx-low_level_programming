c68702e97eceec9f1daeb53e529ac71639310764
gamal <gassuntamal2@gmail.com>
2022-03-31 23:45:28 -0700

    program that prints its name, followed by a new line

---
+++ 0x0A-argc_argv/main.h
@@ -0,0 +1,12 @@
+#ifndef MAIN_H
+#define MAIN_H
+
+/**
+* File: main.h
+* Desc: Header file containing prototypes for all functions
+ *       used in the 0x0A-argc_argv directory.
+ */
+
+int _putchar(char c);
+int main(int argc, char *argv[]);
+#endif
